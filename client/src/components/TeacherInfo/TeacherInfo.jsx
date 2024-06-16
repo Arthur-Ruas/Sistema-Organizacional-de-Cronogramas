72579b170077646abc55fa16087e87e9a0264aa9
Arthur-Ruas <arthurruasfe@gmail.com>
2024-06-15 21:57:20 -0300

    Arthur: Mudanças na interface

--- client/src/components/TeacherInfo/TeacherInfo.jsx
+++ client/src/components/TeacherInfo/TeacherInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import image from '../../assets/profile.png'
 
 const TeacherInfo = ({name, color, observation, days, subjects, showInfo, closeInfo, teacherData, scheduleQtd}) => {
@@ -8,6 +8,45 @@ const TeacherInfo = ({name, color, observation, days, subjects, showInfo, closeI
   horas = Math.floor((teacherData * 112) / 60);          
   min = (teacherData * 112) % 60;
 
+  var primeiroModulo = [];
+  var segundoModulo = [];
+  var terceiroModulo = [];
+  
+  subjects.forEach(subject => {
+    if(subject.Modulo == 1){
+      primeiroModulo = [...primeiroModulo, [subject.Nome, subject.Modulo]]
+    }
+    if(subject.Modulo == 2){
+      segundoModulo = [...segundoModulo, [subject.Nome, subject.Modulo]]
+    }
+    if(subject.Modulo == 3){
+      terceiroModulo = [...terceiroModulo, [subject.Nome, subject.Modulo]]
+    }
+  });
+
+
+  const [tab1State, setTab1State] = useState("flex")
+  const [tab2State, setTab2State] = useState("none")
+  const [tab3State, setTab3State] = useState("none")
+
+  function tab1Open(){
+    setTab1State("flex")
+    setTab2State("none")
+    setTab3State("none")
+  }
+
+  function tab2Open(){
+    setTab1State("none")
+    setTab2State("flex")
+    setTab3State("none")
+  }
+
+  function tab3Open(){
+    setTab1State("none")
+    setTab2State("none")
+    setTab3State("flex")
+  }
+  
   return (
     <div className='teacher-info' style={{display: showInfo}}>
       <div className='teacher-info__content'>
@@ -20,13 +59,45 @@ const TeacherInfo = ({name, color, observation, days, subjects, showInfo, closeI
           <div className='teacher-info__subjects'>
             <h4 className='select-text'>Matérias:</h4>
             <div className='teacher-info__wrapper-subjects'>
-              {
-                subjects.map((subject) =>{
-                  return(
-                    <p>{subject.Nome}</p>
-                  )
-                })
-              }
+              <div className='teacher-info__wrapper-selectors'>
+                <input type='radio' name='aa' id='a11' onClick={tab1Open}/>
+                <label for='a11'>Módulo 1</label>
+
+                <input type='radio' name='aa' id='a22' onClick={tab2Open}/>
+                <label for='a22'>Módulo 2</label>
+
+                <input type='radio' name='aa' id='a33' onClick={tab3Open}/>
+                <label for='a33'>Módulo 3</label>
+              </div>
+              <div>
+                <div className='teacher-info__subject' style={{display: tab1State}}>
+                  {
+                    primeiroModulo.map((info) =>{
+                      return(
+                        <h4>{info[0]}</h4>
+                      )
+                    })
+                  }
+                </div>
+                <div className='teacher-info__subject' style={{display: tab2State}}>
+                  {
+                    segundoModulo.map((info) =>{
+                      return(
+                        <h4>{info[0]}</h4>
+                      )
+                    })
+                  }
+                </div>
+                <div className='teacher-info__subject' style={{display: tab3State}}>
+                  {
+                    terceiroModulo.map((info) =>{
+                      return(
+                        <h4>{info[0]}</h4>
+                      )
+                    })
+                  }
+                </div>
+              </div>
             </div>   
           </div>
         </div>
